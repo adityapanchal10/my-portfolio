a1a32132e5d6e2654ddb58ceb1a11b693d6cc7c1
adityapanchal10 <panchaladitya007@gmail.com>
2024-08-07 23:38:07 +0200

    add more quotes and colors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,12 @@
 import SocialIcons from '@/components/SocialIcons';
 import { memo } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const quotes = [
-  "Perfect moments can be had, but not preserved, except in memory.",
+  'Perfect moments can be had, but not preserved, except in memory...',
+  'May the wind under your wings bear you where the sun sails and the moon walks...',
+  'Life is to dip your toes in cold water and hope it brings you warmth...',
 ];
 
 const getRandomQuote = () => {
@@ -11,8 +15,22 @@ const getRandomQuote = () => {
 };
 
 const Footer = () => {
-  const randomQuote = getRandomQuote();
+  const [randomQuote, setRandomQuote] = useState(getRandomQuote());
   const year: number = new Date().getFullYear();
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setRandomQuote(getRandomQuote());
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <div className='mt-4 flex flex-col items-center py-8 font-light dark:text-white lg:py-8'>
--- lib/hooks/useRandomColorPair.ts
+++ lib/hooks/useRandomColorPair.ts
@@ -5,7 +5,9 @@ export function useRandomColorPair(): Tuple<string> {
   const colors: Tuple<string>[] = [
     ['#F5E1FF', '#CAF0F8'],
     ['#EEEBFF', '#FFFAD4'],
-    ['#FF6F59', '#43AA8B']
+    ['#FF6F59', '#43AA8B'],
+    ['#F5FFC6', '#B4E1FF'],
+    ['#CCFCCB', '#89AAE6'],
   ];
 
   const random = Math.round(Math.random() * (colors.length - 1));
